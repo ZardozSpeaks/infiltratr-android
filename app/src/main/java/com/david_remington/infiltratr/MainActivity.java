0312591c9dd95efd8560dd75ff9928e7c0e55926
David Remington <david.ian.remington@gmail.com>
2016-04-22 15:16:37 -0700

    add initial styling to landing page

---
+++ app/src/main/java/com/david_remington/infiltratr/MainActivity.java
@@ -0,0 +1,46 @@
+package com.david_remington.infiltratr;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.landingPageImageView) ImageView mLandingPageImageView;
+    @Bind(R.id.startButton) Button mStartButton;
+    @Bind(R.id.titleTextView) TextView mTitleTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        Typeface myCustomFont = Typeface.createFromAsset(getAssets(), "Rustico.ttf");
+        mTitleTextView.setTypeface(myCustomFont);
+
+    //Try to get Picasso working on Monday. Background is hardcoded for now
+//        Picasso.with(this)
+//                .load("http://i64.photobucket.com/albums/h164/hewhoiswithoutaname/background_zpsz298orhf.jpg")
+//                .resize(50, 50)
+//                .centerCrop()
+//                .into(mLandingPageImageView);
+
+        mStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MapActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
