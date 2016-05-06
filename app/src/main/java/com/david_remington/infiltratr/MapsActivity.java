bb3f670cf0484ac3b96f6e2e48f59238b14e8aba
David Remington <david.ian.remington@gmail.com>
2016-05-06 12:52:10 -0700

    bugfix for maps activity logic

--- app/src/main/java/com/david_remington/infiltratr/MapsActivity.java
+++ app/src/main/java/com/david_remington/infiltratr/MapsActivity.java
@@ -8,7 +8,6 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
-import android.util.Log;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -18,7 +17,6 @@ import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
@@ -32,19 +30,33 @@ public class MapsActivity extends FragmentActivity implements
     private GoogleMap mMap;
     private GoogleApiClient mGoogleApiClient;
     private LocationRequest mLocationRequest;
+    private Location mLastLocation;
     private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
-    public static final String TAG = MapsActivity.class.getSimpleName();
+
+    /* defines location request for onLocationChange,
+       locates map fragment,
+       issues callback for GoogleMap object*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
+        mLocationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(20000)
+                .setFastestInterval(5000);
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
     }
+
+    /* listens for the result of callback for GoogleMap object,
+       defines member variable as result of callback,
+       checks permissions then enables play location services
+       builds query then issues callback to play location services */
 
     @Override
     public void onMapReady(GoogleMap map) {
@@ -53,6 +65,7 @@ public class MapsActivity extends FragmentActivity implements
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
             mMap.setMyLocationEnabled(true);
+
         }
         else {
             String warning = "You must enable location services in app permissions to view this screen";
@@ -61,37 +74,42 @@ public class MapsActivity extends FragmentActivity implements
         }
 
         buildGoogleApiClient();
+        mGoogleApiClient.connect();
     }
+
+    /* listens for results of callback to play services
+       checks permissions then checks for last known location
+       if last known location is void it calls the method that finds new location*/
 
 
     @Override
     public void onConnected(Bundle bundle) {
-        Location location = null;
-        mLocationRequest = LocationRequest.create()
-                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
-                .setInterval(10 * 1000)
-                .setFastestInterval(1000);
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
-            location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+            if(mLastLocation != null) {
+                mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+                handleNewLocation(mLastLocation);
+            } else {
+               startLocationUpdates();
+            }
         }
         else {
             String warning = "You must enable location services in app permissions to view this screen";
             Toast.makeText(this, warning , Toast.LENGTH_LONG)
                     .show();
         }
-        if(location == null) {
-            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
-        } else {
-            handleNewLocation(location);
-        }
     }
+
+    /* listens to see if connection to play services has been lost*/
 
     @Override
     public void onConnectionSuspended(int i) {
-
+        String warning = "Connection to play services has been lost";
+        Toast.makeText(this, warning , Toast.LENGTH_LONG)
+                .show();
     }
 
+    /* listens to see if connection to play services fails and catches errors*/
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
@@ -109,31 +127,45 @@ public class MapsActivity extends FragmentActivity implements
 
     }
 
-    private void handleNewLocation(Location location) {
-        double currentLatitude = location.getLatitude();
-        double currentLongitude = location.getLongitude();
-        LatLng latLng = new LatLng(currentLatitude, currentLongitude);
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-    }
-
+    /* listens for results of location update callback
+       redefines location member variable based on results
+       invokes location handler method to move center map view and zoom*/
 
     @Override
     public void onLocationChanged(Location location) {
+        mLastLocation = location;
         handleNewLocation(location);
     }
 
 
+    /* centers the map view and zooms camera on location */
 
-    private void buildGoogleApiClient() {
+    private void handleNewLocation(Location location) {
+        double currentLatitude = location.getLatitude();
+        double currentLongitude = location.getLongitude();
+        LatLng latLng = new LatLng(currentLatitude, currentLongitude);
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15.0f));
+    }
+
+    /* constructs the play services callback client*/
+
+    protected synchronized void buildGoogleApiClient() {
 
         mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
                 .addApi(LocationServices.API)
                 .build();
-        mGoogleApiClient.connect();
 
     }
 
+    /* issues callback to play services based on location request builder*/
 
+    protected void startLocationUpdates() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        }
+    }
 
 }
