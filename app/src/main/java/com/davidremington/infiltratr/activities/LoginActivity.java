32401061f00a4e6498ce6d6373b2bbb7efa181cb
David Remington <david.ian.remington@gmail.com>
2017-09-25 12:24:30 -0700

    rename MainActivity to LoginActivity

--- app/src/main/java/com/davidremington/infiltratr/activities/LoginActivity.java
+++ app/src/main/java/com/davidremington/infiltratr/activities/LoginActivity.java
@@ -11,17 +11,17 @@ import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
-import butterknife.Bind;
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 import com.davidremington.infiltratr.R;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
-    @Bind(R.id.landingPageImageView) ImageView mLandingPageImageView;
-    @Bind(R.id.startButton) Button mStartButton;
-    @Bind(R.id.titleTextView) TextView mTitleTextView;
+    @BindView(R.id.landingPageImageView) ImageView landingPageBackgroundImage;
+    @BindView(R.id.startButton) Button mStartButton;
+    @BindView(R.id.titleTextView) TextView mTitleTextView;
 
     private static final String FONT = "Rustico.ttf";
     private static final String BACKGROUND_ROOT_URL = "http://i64.photobucket.com/albums/h164/hewhoiswithoutaname";
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
 
         Typeface myCustomFont = Typeface.createFromAsset(getAssets(), FONT);
@@ -40,13 +40,13 @@ public class MainActivity extends AppCompatActivity {
                 .load(getBackgroundUrl())
                 .resize(800, 500)
                 .centerCrop()
-                .into(mLandingPageImageView);
+                .into(landingPageBackgroundImage);
 
     }
 
     @OnClick(R.id.startButton)
     public void onClick(View view) {
-        Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+        Intent intent = new Intent(LoginActivity.this, MapsActivity.class);
         startActivity(intent);
     }
 
